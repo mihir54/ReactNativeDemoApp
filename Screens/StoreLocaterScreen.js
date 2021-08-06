3fd20555ea229904d4cac264021b745e1e192548
Mihir Prajapati <mihir.prajapati@krishtechnolabs.com>
2021-08-06 11:00:12 +0530

    Configure google map.Tooltip on marker

--- Screens/StoreLocaterScreen.js
+++ Screens/StoreLocaterScreen.js
@@ -1,19 +1,255 @@
 import React from 'react';
-import { View, Text,StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
+import MapView, { Callout, Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+import { useTheme } from '@react-navigation/native';
+
+const mapDarkStyle = [
+    {
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#212121"
+            }
+        ]
+    },
+    {
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#757575"
+            }
+        ]
+    },
+    {
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "color": "#212121"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#757575"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative.country",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#9e9e9e"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative.land_parcel",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative.locality",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#bdbdbd"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#757575"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#181818"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#616161"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "color": "#1b1b1b"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#2c2c2c"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#8a8a8a"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#373737"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#3c3c3c"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway.controlled_access",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#4e4e4e"
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#616161"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#757575"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#000000"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#3d3d3d"
+            }
+        ]
+    }
+];
+
+const mapStandardStyle = [
+    {
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+];
 
 const StoreLocaterScreen = () => {
-    return(
-        <View style={styles.container}>
-            <Text style={styles.text}>StoreLocater Screen </Text>
+    const theme = useTheme();
+    return (
+        <View >
+            <MapView
+                provider={PROVIDER_GOOGLE}
+                style={styles.map}
+                customMapStyle={theme.dark ? mapDarkStyle : mapStandardStyle}
+                region={{
+                    latitude: 23.0225,
+                    longitude: 72.5714,
+                    latitudeDelta: 0.015,
+                    longitudeDelta: 0.0121
+                }}
+            >
+                <Marker
+                    coordinate={{
+                        latitude: 23.0225,
+                        longitude: 72.5714,
+                    }}
+                    image={require('../assests/map_marker.png')}
+                    title="My City"
+                    description="My Area"
+                >
+                    <Callout tooltip>
+                        <View>
+
+                            <View style={styles.bubble}>
+                                <Text style={styles.name}>Favourite Store</Text>
+                                <Image
+                                    style={styles.image}
+                                    source={require('../assests/banners/banner3.jpg')}
+                                />
+                            </View>
+                            <View style={styles.arrowBorder}></View>
+                            <View style={styles.arrow}></View>
+                        </View>
+                    </Callout>
+                </Marker>
+            </MapView>
+
         </View>
     );
 };
+
 export default StoreLocaterScreen;
 
 const styles = StyleSheet.create({
     container: {
-        justifyContent: 'center',
-        alignItems: 'center',
         padding: 20,
         paddingTop: 50,
         backgroundColor: '#f9fafd',
@@ -25,4 +261,42 @@ const styles = StyleSheet.create({
         marginBottom: 10,
         color: '#051d5f',
     },
+    map: {
+        height: "100%"
+    },
+    arrow: {
+        backgroundColor: 'transparent',
+        borderColor: 'transparent',
+        borderTopColor: '#fff',
+        borderWidth: 16,
+        alignSelf: 'center',
+        marginTop: -32
+    },
+    arrowBorder: {
+        backgroundColor: 'transparent',
+        borderColor: 'transparent',
+        borderTopColor: '#007a87',
+        borderWidth: 16,
+        alignSelf: 'center',
+        marginTop: -0.5
+    },
+    name: {
+        fontSize: 16,
+        marginBottom: 5
+    },
+    image: {
+        width: 120,
+        height: 80
+    },
+    bubble: {
+        flexDirection: 'column',
+        alignSelf: 'flex-start',
+        backgroundColor: '#fff',
+        borderColor: '#ccc',
+        borderWidth: 0.5,
+        borderRadius: 6,
+        padding: 15,
+        width: 150,
+    }
+
 });
